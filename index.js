7b4fda6006cbdb341e4f1f7db9fda836b09517c6
unknown <Kokonin.1@seznam.cz>
2024-09-22 15:31:15 +0200

    admin dashboard functionality

--- index.js
+++ index.js
@@ -2,26 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const firebase = require("firebase-admin")
+const admin = require("firebase-admin")
+const credentials = require("./firebase.json");
 
-const serviceAccount = {
-    "type": "service_account",
-    "project_id": process.env.FB_PROJ_ID,
-    "private_key_id": process.env.FB_PRIVATE_KEY_ID,
-    "private_key": process.env.FB_PRIVATE_KEY,
-    "client_email": process.env.FB_CLIENT_EMAIL,
-    "client_id": process.env.FB_CLIENT_ID,
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": process.env.FB_CLIENT_CERT_URL
-};
-firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serviceAccount),
-    databaseURL: "https://deveclipse-5ad77-default-rtdb.europe-west1.firebasedatabase.app"
+admin.initializeApp({
+    credential: admin.credential.cert(credentials)
 });
-const db = firebase.firestore();
-console.log(db);
 
 const app = express();
 app.use(express.json());
@@ -57,17 +43,7 @@ app.get("/create_post", async (req, res) => {
     const content = req.query.content;
 
     if (password === process.env.ADMIN_PASSWORD) {
-        try {
-            const newPost = {
-                title: title,
-                content: content,
-                createdAt: firebase.firestore.FieldValue.serverTimestamp()
-            };
-            const docRef = await db.collection("posts").add(newPost);
-            res.status(200).send(`Post created with ID: ${docRef.id}`);
-        } catch (error) {
-            res.status(500).send("Error creating post: " + error.message);
-        }
+        
     } else {
         res.status(403).send("Forbidden");
     }
