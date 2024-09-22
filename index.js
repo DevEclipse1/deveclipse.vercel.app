4cc4e6a89c5333b6132294bf49a8dabd45bc5bb1
unknown <Kokonin.1@seznam.cz>
2024-09-22 13:10:00 +0200

    Added index.html

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const express = require("express")
+const fs = require("node:fs")
 
 const app = express();
 app.use(express.json());
 
 app.get("*",async(req, res) => {
-    res.send("Hello, World!");
+    let p = path.join(process.cwd(), 'index.html');
+    let file = fs.readFileSync(p);
+    res.send(file);
 });
 
 app.listen(80, function(err)
