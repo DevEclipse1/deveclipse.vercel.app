e4d90c27cc099f64dd69b09103e4c8ff9fce54a9
unknown <Kokonin.1@seznam.cz>
2024-09-22 15:15:43 +0200

    admin dashboard functionality

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const fs = require("fs");
 const path = require("path");
 const firebase = require("firebase-admin")
 
-const serviceAccount = require(path.join(process.cwd(), "firebase.json"));
-const firebase_config = {
-    apiKey:process.env.FB_API_KEY,
-    authDomain:process.env.FB_AUTH_DOMAIN,
-    projectId:process.env.FB_PROJ_ID,
-    storageBucket:process.env.FB_STORAGE_BUCKET,
-    messagingSenderId:process.env.FB_MSG_SENDER_ID,
-    appId:process.env.FB_APP_ID,
-    measurementId:process.env.FB_MEASURE_ID,
-    databaseURL:"https://deveclipse-5ad77-default-rtdb.europe-west1.firebasedatabase.app"
+const serviceAccount = {
+    "type": "service_account",
+    "project_id": process.env.FB_PROJ_ID,
+    "private_key_id": process.env.FB_PRIVATE_KEY_ID,
+    "private_key": process.env.FB_PRIVATE_KEY,
+    "client_email": process.env.FB_CLIENT_EMAIL,
+    "client_id": process.env.FB_CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": process.env.FB_CLIENT_CERT_URL
 };
 firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert(serviceAccount),
